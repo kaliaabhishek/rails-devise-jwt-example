1143127be7cccd50b2892e08270ff22f47026e53
Abhishek Kalia <abhishek.kalia@outlook.com>
2019-01-27 15:30:08 +0530

    Add knock + devise auth

--- Gemfile
+++ Gemfile
@@ -43,3 +43,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Handles authentication 
+gem 'devise'
+
+# Handles JWTs
+gem 'knock'
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_many :posts
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  alias_method :authenticate, :valid_password?
+
+  def self.from_token_payload(payload)
+    self.find payload["sub"]
+  end
+end
